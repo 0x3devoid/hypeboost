e535383720048739d5b41b0771641ed7ef0849fe
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2025-05-10 13:00:15 +0100

    were

--- src/app/utils/ConnectWalletButton.tsx
+++ src/app/utils/ConnectWalletButton.tsx
@@ -13,7 +13,7 @@ const ConnectWalletButton = () => {
       onClick={isConnected ? disconnectWallet : connectWallet}
       disabled={isConnecting}
       type='button'
-      className="flex items-center justify-center text-center gap-2 bg-blue-600 hover:bg-blue-700 transition-colors px-2 py-1 rounded-lg text-black text-[14px] cursor-pointer mr-3"
+      className="flex items-center justify-center text-center gap-2 bg-blue-600 hover:bg-blue-700 transition-colors px-5 py-1 rounded-lg text-white  cursor-pointer mr-3"
     >
       <Wallet size={15} />
       {isConnecting ? (
